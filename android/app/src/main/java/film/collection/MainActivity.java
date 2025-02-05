8f52820092d55ca1ed822a3001aa08d1b7e5dfb5
NPT <nptvscode@gmail.com>
2025-02-05 02:25:28 +0000

    initialize project structure with essential configuration files and components

---
+++ android/app/src/main/java/film/collection/MainActivity.java
@@ -0,0 +1,5 @@
+package film.collection;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'film.collection',
+  appName: 'film-collection',
+  webDir: 'dist'
+};
+
+export default config;
